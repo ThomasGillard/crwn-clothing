79284eeb2bca9832d27b8fd2849edba457febd2b
ThomasGillard <38733378+ThomasGillard@users.noreply.github.com>
2019-12-26 13:10:06 -0500

    update menuitems withRouter HOC

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import {withRouter} from 'react-router-dom';
 
 import './menu-item.style.scss';
 
-const MenuItem = ({ title, imageUrl, size }) => (
-    <div  className={`${size} menu-item`}>
+const MenuItem = ({ title, imageUrl, size, history }) => (
+    <div  className={`${size} menu-item`} onClick ={()=>history.push()}>
         <div className='background-image' 
         style={{backgroundImage:`url(${imageUrl})` }}>
 
@@ -15,5 +16,5 @@ const MenuItem = ({ title, imageUrl, size }) => (
     </div>
 );
 
-export default MenuItem;
+export default withRouter(MenuItem);
 
